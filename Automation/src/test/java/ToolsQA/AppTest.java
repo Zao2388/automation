24dcf2797de21acaa2d535913504ca8e19e6490b
Roberto Serrano <rserrano@growthaccelerationpartners.com>
2015-08-04 11:34:35 -0600

    Adding TestNG and Selenium

--- Automation/src/test/java/ToolsQA/AppTest.java
+++ Automation/src/test/java/ToolsQA/AppTest.java
@@ -1,38 +1,65 @@
 package ToolsQA;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest{
+    private WebDriver driver;
+
+    public void setupSelenium(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @Test
+    //Como hago para pasarle el URL desde XML
+    //@Parameters{url=}
+    public void testSearch(){
+        /**
+         * http://www.iluv2code.com/test-project-selenium2-testng-maven.html
+         */
+/*        driver.navigate().to("http://staging.businessuites.com");
+        driver.findElement(By.cssSelector("#Inquire_first_name")).clear();
+        driver.findElement(By.cssSelector("#Inquire_first_name")).sendKeys("Roberto Test");
+        driver.findElement(By.cssSelector("#Inquire_last_name")).clear();
+        driver.findElement(By.cssSelector("#Inquire_last_name")).sendKeys("Test");
+        driver.findElement(By.cssSelector("#Inquire_company")).clear();
+        driver.findElement(By.cssSelector("#Inquire_company")).sendKeys("GAP");
+        driver.findElement(By.cssSelector("#Inquire_phone")).clear();
+        driver.findElement(By.cssSelector("#Inquire_phone")).sendKeys("1234567895");
+        driver.findElement(By.cssSelector("#Inquire_email")).clear();
+        driver.findElement(By.cssSelector("#Inquire_email")).sendKeys("rserrano@wearegap.com");
+
+        driver.findElement(By.cssSelector("#btn_inquire_55c0ed26df3dd")).click();
+        assertEquals("this","this","this");*/
+        driver.navigate().to("http://www.iluv2code.com");
+
+        String searchTerm = "Java";
+
+        //Clear the search box and ype searchTerm
+        driver.findElement(By.cssSelector("#s")).clear();
+        driver.findElement(By.cssSelector("#s")).sendKeys(searchTerm);
+
+        //Click on the magnifying glass
+        driver.findElement(By.cssSelector("#searchsubmit")).click();
+
+        //Get search term for search results, to check if matches the given search
+        String termInTitle =
+                driver.findElement(By.cssSelector("#archive-title>strong")).getText();
+
+        assertEquals(termInTitle, searchTerm,
+                "Search term not found in search results");
+    }
+    public void closeSelenium(){
+        driver.quit();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
